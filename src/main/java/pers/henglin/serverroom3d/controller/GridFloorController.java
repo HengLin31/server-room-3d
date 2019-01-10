7110f956f8bc01d554f1cd9eb06e6fb9f62fec8a
Heng Lin <enjoymycodinglife@gmail.com>
2019-01-10 10:43:05 +0800

    use @PathVariable

--- src/main/java/pers/henglin/serverroom3d/controller/GridFloorController.java
+++ src/main/java/pers/henglin/serverroom3d/controller/GridFloorController.java
@@ -1,20 +1,20 @@
 package pers.henglin.serverroom3d.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pers.henglin.serverroom3d.domain.GridFloor;
 import pers.henglin.serverroom3d.mapper.GridFloorMapper;
 
 @RestController
+@RequestMapping("/grid-floor")
 public class GridFloorController {
     @Autowired
     private GridFloorMapper gridFloorMapper;
 
-    @RequestMapping("/getGridFloor")
-    public GridFloor getGridFloor(@RequestParam("id") String id) {
-        GridFloor gridFloor = gridFloorMapper.findById(id);
-        return gridFloor;
+    @RequestMapping("/get/{id}")
+    public GridFloor get(@PathVariable String id) {
+        return gridFloorMapper.findById(id);
     }
 }
